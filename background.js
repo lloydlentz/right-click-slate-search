096c8f0e35a810d74b90e81177d98891e065faa3
Lloyd Lentz <cledwyn@gmail.com>
2020-06-12 17:23:35 -0500

    Fix Regex

--- background.js
+++ background.js
@@ -15,8 +15,12 @@ function onClickHandler(info, tab) {
   }
 //  console.log("domain: " + rtClickDomain);
   if (rlClickDomain){
+//    var punctuation = '!"#$%&\'()*+,-./:;<=>?[\\]^_`{|}~';
+    var regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+    var cleanString = sText.replace(regex, '');
+
     console.log('Slate user domain for Right-Click extension currently is ' + rlClickDomain);
-    var url = 'https://' + rlClickDomain + '/manage/lookup/search#{%22q%22:%22'  + encodeURIComponent(sText) + '%22,%22pg%22:%220%22,%22sort%22:%22%22,%22filter%22:[]}';
+    var url = 'https://' + rlClickDomain + '/manage/lookup/search#{%22q%22:%22'  + encodeURIComponent(cleanString) + '%22,%22pg%22:%220%22,%22sort%22:%22%22,%22filter%22:[]}';
     window.open(url, '_blank');
   } else {
     chrome.tabs.create({ 'url': 'chrome://extensions/?options=' + chrome.runtime.id });
